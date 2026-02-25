407954bd35e668c73bdbbae58789f689635a53e6
Yuriekokubu <60240233118@udru.ac.th>
2026-02-25 12:08:38 +0700

    fix serve prod test

--- playwright.config.js
+++ playwright.config.js
@@ -45,7 +45,7 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
       webServer: {
         // start both the auth server and the static file server
-        command: 'npm run serve:prod',
+        command: 'npm run serve:prod:test',
         url: 'http://localhost:4173',
         reuseExistingServer: !process.env.CI,
       },
